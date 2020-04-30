b3d785ac54158d067d991f6763366abe8f0c3cfe
Igor Machado <igor.machado@gmail.com>
2020-04-30 01:28:47 -0300

    info on readme

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# kahan-float-cpp
-Kahan Floating-Point in C++
+# kahan-float
+## Kahan Floating-Point (in C++)
+
+This project provides simple implementation of a float type with Kahan Summation strategy.
+
+
+## How to use?
+
+Just `#include "kahan.hpp"`, a single header library.
+There are two basic types:
+
+- `kahan::kfloat32`, equivalent to `float`
+- `kahan::kfloat64`, equivalent to `double`
+
+One example is to add `30` times the value `0.1`, result should be `3.0` (but it's only correct on `kfloat32`):
+
+```cpp
+#include "kahan.hpp"
+using kahan::kfloat32;
+
+int main() {
+    // add 30 times 0.1 (should be 3.0)
+    float x = 0;
+    int k = 30;
+    while (k--)
+        x += 0.1f;
+    printf("x=%f\n", x);        // outputs 2.999999
+
+    kfloat32 y = 0;
+    k = 30;
+    while (k--)
+        y += 0.1f;
+    printf("y=%f\n", (float)y); // outputs 3.000000
+    // ...
+```
+
+
+## License
+
+MIT License
--- src/main_test.cpp
+++ src/main_test.cpp
@@ -3,7 +3,10 @@
 #include "kahan.hpp"
 
 using namespace std;
-using namespace kahan;
+//using namespace kahan;
+
+using kahan::kfloat32;
+using kahan::kfloat64;
 
 // ==========================================
 //           very basic testing
@@ -25,11 +28,35 @@ main()
    cout << kf << endl;
 
    float vf_1 = 1.0;
-   print_IEEE754(vf_1);
+   kahan::print_IEEE754(vf_1);
    float vf_2 = -2.0;
-   print_IEEE754(vf_2);
+   kahan::print_IEEE754(vf_2);
    float vf_875 = 0.875;
-   print_IEEE754(vf_875);
+   kahan::print_IEEE754(vf_875);
+
+   float x = 0;
+   int k = 30;
+   while (k--)
+      x += 0.1f;
+   printf("x=%f\n", x); // 2.999999
+
+   kfloat32 y = 0;
+   k = 30;
+   while (k--)
+      y += 0.1f;
+   printf("y=%f\n", (float)y); // 3.000000
+
+   double x2 = 0;
+   k = 1000;
+   while (k--)
+      x2 += 0.1f;
+   printf("x2=%f\n", x2); // 100.000001
+
+   kfloat64 y2 = 0;
+   k = 1000;
+   while (k--)
+      y2 += 0.1f;
+   printf("y2=%f\n", (double)y2); // 100.000000
 
    return 0;
 }

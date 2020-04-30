0a90c37a0d861c364eb90f40fd57909ede21095e
Igor Machado <igor.machado@gmail.com>
2020-04-29 23:55:32 -0300

    test basic precision kfloat32

--- tests/kahan.test.cpp
+++ tests/kahan.test.cpp
@@ -23,3 +23,19 @@ TEST_CASE("Kahan Tests kfloat32(1.0 + 1.0) == 2.0")
    kf32 += f;
    REQUIRE((float)kf32 == 2.0);
 }
+
+TEST_CASE("Kahan Tests 20x float32 0.1 <> 2.0")
+{
+   float f = 0.0f;
+   for (unsigned i = 0; i < 20; i++)
+      f += 0.1f;
+   REQUIRE(f != 2.0);
+}
+
+TEST_CASE("Kahan Tests 20x kfloat32 0.1 == 2.0")
+{
+   kfloat32 f = 0.0f;
+   for (unsigned i = 0; i < 20; i++)
+      f += 0.1f;
+   REQUIRE((float)f == 2.0);
+}

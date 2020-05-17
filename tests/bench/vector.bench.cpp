dfd7aa090035bbc57ad28404b16bf6ae853f6ed9
Igor Machado <igor.machado@gmail.com>
2020-05-17 02:30:24 -0300

    some benchmark structure

--- tests/bench/vector.bench.cpp
+++ tests/bench/vector.bench.cpp
@@ -1,37 +1,73 @@
-#include<iostream>
-
+//#include<iostream>
 #include <benchmark/benchmark.h>
 #include <vector>
 
+// CppCon 2015: Chandler Carruth "Tuning C++: Benchmarks, and CPUs, and Compilers! Oh My!"
+// https://www.youtube.com/watch?v=nXaxk27zwlk
+static void escape(void *p)
+{
+   asm volatile("": : "g"(p) : "memory");
+}
+//
+static void clobber()
+{
+   asm volatile("": : : "memory");
+}
+// ===============================
+//  library seems to have now: 
+//    benchmark::DoNotOptimize(x)
+//    benchmark::ClobberMemory()
+// -------------------------------
+
 static void bench_create(benchmark::State &state)
 {
-   while(state.KeepRunning())
+   //while(state.KeepRunning())
+    for (auto _ : state) 
    {
       std::vector<int> v;
+      escape(&v);
       (void)v;
    }
 }
 BENCHMARK(bench_create);
 
+
 static void bench_reserve(benchmark::State &state)
 {
-   while(state.KeepRunning())
+   //while(state.KeepRunning())
+    for (auto _ : state) 
    {
       std::vector<int> v;
       v.reserve(1);
+      escape(v.data());
    }
 }
 BENCHMARK(bench_reserve);
 
 static void bench_push_back(benchmark::State &state)
 {
-   while(state.KeepRunning())
+   //while(state.KeepRunning())
+   for (auto _ : state) 
    {
       std::vector<int> v;
       v.reserve(1);
+      escape(v.data());
       v.push_back(42);
+      clobber();
    }
 }
 BENCHMARK(bench_push_back);
+
+static void bench_norev_push_back(benchmark::State &state)
+{
+   //while(state.KeepRunning())
+   for (auto _ : state) 
+   {
+      std::vector<int> v;
+      v.push_back(42);
+      clobber();
+   }
+}
+BENCHMARK(bench_norev_push_back);
 
 BENCHMARK_MAIN();
--- tests/get_gbenchmark.sh
+++ tests/get_gbenchmark.sh
@@ -5,11 +5,15 @@ CMAKE=cmake
 # refresh submodules
 git submodule update --init --recursive
 git submodule update --recursive
-# build thirdparty deps
-#if [ ! -d "thirdparty/gbenchmark/googletest" ] ; then
-#    (cd thirdparty && git clone https://github.com/google/googletest.git gbenchmark/googletest)
-#fi
-#export CXX=/usr/local/bin/g++
-#(cd thirdparty && cd gbenchmark && rm -rf build && mkdir build && cd build && cmake -DBENCHMARK_DOWNLOAD_DEPENDENCIES=ON -DCMAKE_BUILD_TYPE=Release -DBENCHMARK_ENABLE_LTO=true .. && make )
+#
+###############
+# USING CLANG
+#
+#(export CXX=/usr/bin/clang++ && cd thirdparty && cd gbenchmark && rm -rf build && mkdir build && cd build && cmake -DBENCHMARK_DOWNLOAD_DEPENDENCIES=ON -DCMAKE_BUILD_TYPE=Release -DBENCHMARK_ENABLE_LTO=true .. && make )
+# ... /libbenchmark.a: erro ao adicionar símbolos: Formato de ficheiro não reconhecido
+#
+###############
+# USING GCC
+#
 # LTO not working on any gcc: -DBENCHMARK_ENABLE_LTO=true
-(cd thirdparty && cd gbenchmark && rm -rf build && mkdir build && cd build && $CMAKE -DBENCHMARK_DOWNLOAD_DEPENDENCIES=ON -DCMAKE_BUILD_TYPE=Release .. && make )
+(export CXX=/usr/bin/g++ && cd thirdparty && cd gbenchmark && rm -rf build && mkdir build && cd build && $CMAKE -DBENCHMARK_DOWNLOAD_DEPENDENCIES=ON -DCMAKE_BUILD_TYPE=Release .. && make )
